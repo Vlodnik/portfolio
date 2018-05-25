1517be9142cf58aa8fdf923841fc96717b63c344
Evan Thompson <evanbrowningthompson@gmail.com>
2018-05-25 15:54:17 -0400

    made menu functional

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function toggleMenu(e) {
   e.preventDefault();
-  let toggled = $('.dropdown').hasClass('is-active');
+  const toggled = $('.dropdown').hasClass('is-active');
   if (toggled) {
     $('.dropdown').removeClass('is-active');
   } else {
@@ -8,14 +8,70 @@ function toggleMenu(e) {
   }
 }
 
+function scrollToTop(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop: 0});
+  if($('.dropdown').hasClass('is-active')) {
+    toggleMenu(e);
+  }
+}
+
+function scrollToProjects(e) {
+  e.preventDefault();
+  const scrollCorrection = window.innerHeight / 10 - 2;
+  $('html, body').animate({scrollTop: $('#projects').offset().top - scrollCorrection});
+
+  if($('.dropdown').hasClass('is-active')) {
+    toggleMenu(e);
+  }
+}
+
+function scrollToAbout(e) {
+  e.preventDefault();
+  const scrollCorrection = window.innerHeight / 10 - 2;
+  $('html, body').animate({scrollTop: $('#about').offset().top - scrollCorrection});
+
+  if($('.dropdown').hasClass('is-active')) {
+    toggleMenu(e);
+  }
+}
+
+function scrollToContact(e) {
+  e.preventDefault();
+  const scrollCorrection = window.innerHeight / 10 - 2;
+  $('html, body').animate({scrollTop: $('#contact').offset().top - scrollCorrection});
+
+  if($('.dropdown').hasClass('is-active')) {
+    toggleMenu(e);
+  }
+}
+
 function handleMenuToggle() {
   $('#hamburger').click(toggleMenu);
 }
 
-function handle
+function handleScrollToTop() {
+  $('.home').click(scrollToTop);
+}
+
+function handleScrollToProjects() {
+  $('.dropdown li:first-child').click(scrollToProjects);
+}
+
+function handleScrollToAbout() {
+  $('.dropdown li:nth-child(2)').click(scrollToAbout);
+}
+
+function handleScrollToContact() {
+  $('.dropdown li:nth-child(3)').click(scrollToContact);
+}
 
 function setupListeners() {
   handleMenuToggle();
+  handleScrollToTop();
+  handleScrollToProjects();
+  handleScrollToAbout();
+  handleScrollToContact();
 }
 
 setupListeners();
